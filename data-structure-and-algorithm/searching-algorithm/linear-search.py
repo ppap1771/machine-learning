ccbc2d5a517afd4fb10ed875c2a009e13a20ce63
TR0UBLESH00TER <74814583+TR0UBLESH00TER@users.noreply.github.com>
2021-02-19 09:19:18 +0530

    Update

--- data-structure-and-algorithm/searching-algorithm/linear-search.py
+++
@@ -1,8 +0,0 @@
-# Linear Search - Sorted Array
-
-def search(Array,Element):
-    """Linear Search Algorithm implemented in Python3"""
-    for i in range(0,len(Array)):
-        if Array[i] == Element:
-            return i
-    return -1
